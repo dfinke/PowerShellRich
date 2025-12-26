4805cd43758c7fc85ef87fb3d9ba0f6bd2c8807f
dfinke <finked@hotmail.com>
2025-12-26 17:41:40 -0500

    Fix module import path in 11_SimpleLayout.ps1 for correct script execution

--- Examples/11_SimpleLayout.ps1
+++ Examples/11_SimpleLayout.ps1
@@ -1,10 +1,10 @@
-Import-Module ".\PowerShellRich.psd1" -Force
+Import-Module "$PSScriptRoot\..\PowerShellRich.psd1" -Force
 
 $layout = New-RichLayout -Name "root"
 Split-RichLayout -Layout $layout -Direction "Horizontal" -Names "left", "right"
 
-Update-RichLayout -Layout $layout -Name "left" -Content "Left Side"
-Update-RichLayout -Layout $layout -Name "right" -Content "Right Side"
+$null = Update-RichLayout -Layout $layout -Name "left" -Content "Left Side"
+$null = Update-RichLayout -Layout $layout -Name "right" -Content "Right Side"
 
 $rendered = Format-RichLayout -Layout $layout -Width 40 -Height 5
 $rendered | ForEach-Object { Write-Host $_ }
